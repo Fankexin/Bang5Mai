6cd4ab72b02ae2ed7fe112135ac9ec66939e7e3b
范珂欣 <33643127+Fankexin@users.noreply.github.com>
2018-12-23 20:27:13 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -172,50 +172,116 @@ one.style.right = '-89px';
 two.style.right = '-89px';
 three.style.right = '-89px';
 four.style.right = '-89px';
-one.setAttribute("onmouseover","numPlus(one)");
-one.setAttribute("onmouseout","numMinus(one)");
-two.setAttribute("onmouseover","numPlus(two)");
-two.setAttribute("onmouseout","numMinus(two)");
-three.onmouseover = function (){
-	three11.src="./img/erwei.png";
-    three11.classList.remove("three11");
-    three11.classList.add("three111");
-    numPlus(three);
+var time1;
+one.onmouseover = function () {
+    var t = parseInt(one.style.right);
+    clearInterval(time1);
+    time1 = setInterval(function (){
+        t++;
+        one.style.right = t + 'px';
+        if(t >= 0){
+            one.style.right = '0px';
+            clearInterval(time1);
+        }
+        },0.1);
 }
-three.onmouseout = function (){
-	three11.src="./img/serwei.png";
-    three11.classList.remove("three111");
-    three11.classList.add("three11");
-    numMinus(three);   
+one.onmouseout = function () {
+    var t = parseInt(one.style.right);
+    clearInterval(time1);
+    time1 = setInterval(function (){
+        t--;
+        one.style.right = t + 'px';
+        if(t <= -89){
+            one.style.right = '-89px';
+            clearInterval(time1);
+        }
+        },0.1);
 }
 
-four.setAttribute("onmouseover","numPlus(four)");
-four.setAttribute("onmouseout","numMinus(four)");
+var time2;
+two.onmouseover = function () {
+    var t = parseInt(two.style.right);
+    clearInterval(time2);
+    time2 = setInterval(function (){
+        t++;
+        two.style.right = t + 'px';
+        if(t >= 0){
+            two.style.right = '0px';
+            clearInterval(time2);
+        }
+    },0.1);
+}
+two.onmouseout = function () {
+    var t = parseInt(two.style.right);
+    clearInterval(time2);
+    time2 = setInterval(function (){
+        t--;
+        two.style.right = t + 'px';
+        if(t <= -89){
+            two.style.right = '-89px';
+            clearInterval(time2);
+        }
+    },0.1);
+}
 
-function numPlus(num){
-	var t;
-	if(num.style.right == '-89px'){
-		var right = parseInt(num.style.right);
-		t = setInterval(function(){
-			right = right + 1;
-			num.style.right = right +"px";		
-			if(num.style.right == '0px'){
-				clearInterval(t);
-			}
-		}, 0.1);	
-	}	
-};
+var time3;
+three.onmouseover = function () {
+    var t = parseInt(three.style.right);
+    clearInterval(time3);
+    time3 = setInterval(function (){
+        t++;
+        three.style.right = t + 'px';
+        if(t >= -40){
+            three11.src="./img/erwei.png";
+            three11.classList.remove("three11");
+            three11.classList.add("three111");
+        }
+        if(t >= 0){
+            three.style.right = '0px';
+            clearInterval(time3);
+        }
+    },0.1);
+}
+three.onmouseout = function () {
+    var t = parseInt(three.style.right);
+    clearInterval(time3);
+    time3 = setInterval(function (){
+        t--;
+        three.style.right = t + 'px';
+        if(t <= -40){
+            three11.src="./img/serwei.png";
+            three11.classList.remove("three111");
+            three11.classList.add("three11");
+        }
+        if(t <= -89){
+            three.style.right = '-89px';
+            clearInterval(time3);
+        }
+    },0.1);
+}
 
-function numMinus(num){
-	var t;
-	if(num.style.right == '0px'){
-		var right = parseInt(num.style.right);
-		t = setInterval(function(){
-			right = right - 1;
-			num.style.right = right +"px";		
-			if(num.style.right == '-89px'){
-				clearInterval(t);
-			}
-		}, 0.1);	
-	}	
-};
+var time4;
+four.onmouseover = function () {
+    var t = parseInt(four.style.right);
+    clearInterval(time4);
+    time4 = setInterval(function (){
+        t++;
+        four.style.right = t + 'px';
+        if(t >= 0){
+            four.style.right = '0px';
+            clearInterval(time4);
+        }
+    },0.1);
+}
+four.onmouseout = function () {
+    var t = parseInt(four.style.right);
+    clearInterval(time4);
+    time4 = setInterval(function (){
+        t--;
+        four.style.right = t + 'px';
+        if(t <= -89){
+            four.style.right = '-89px';
+            clearInterval(time4);
+        }
+    },0.1);
+}
